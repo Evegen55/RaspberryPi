79e0c729479af20403d3854fca5a24fcec936a81
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-02-09 18:54:11 +0300

    try it on raspberry pi

--- RoboticArm/MainControlWindow.py
+++ RoboticArm/MainControlWindow.py
@@ -4,7 +4,7 @@ Created on Jan 31, 2017
 @author: Evgenii_Lartcev
 '''
 from Tkinter import *
-#from stepper_L293_bipolar import *
+from stepper_L293_bipolar import *
 
 # GUI definition
 root = Tk()
@@ -28,6 +28,8 @@ def updateAndPrintValues(duty):
     # print varDelay.get() #for test purpose
     return
 
+# TODO improve functions that control stepper - pass PINs as paramethers or pass number of motor and so on
+
 # for 1 step to left or right if button fired once
 # for many steps if button fired longer time
 # get delay from the scale
@@ -36,23 +38,25 @@ def rotate_left():
     delay_in_millisecs_as_string = varDelay.get()
     delay_in_secs_as_float = float (varDelay.get()) /1000    
     button_rotate_left.configure(repeatinterval = delay_in_millisecs_as_string)
-    #backwards(delay_in_secs_as_float, 1)
-    print 'left ' , varDelay.get(), delay_in_millisecs_as_string, delay_in_secs_as_float #for test purpose
+    backwards(delay_in_secs_as_float, 1)
+    #print 'left ' , varDelay.get(), delay_in_millisecs_as_string, delay_in_secs_as_float #for test purpose
 
 def rotate_right():
     delay_in_millisecs_as_string = varDelay.get()
     delay_in_secs_as_float = float (varDelay.get()) /1000
     
     button_rotate_right.configure(repeatinterval = delay_in_millisecs_as_string)
-    #forward(delay_in_secs_as_float, 1)
-    print 'right ' , varDelay.get(), delay_in_millisecs_as_string, delay_in_secs_as_float #for test purpose
+    forward(delay_in_secs_as_float, 1)
+    #print 'right ' , varDelay.get(), delay_in_millisecs_as_string, delay_in_secs_as_float #for test purpose
 
 
 def enable_motor_X():
     if varCheckMotor_X_Enable.get() == 1:
-        print 'gdsfgds'
+        enable_motor()
+        #print 'gdsfgds'
     if varCheckMotor_X_Enable.get() == 0:
-        print '========='
+        disable_motor()
+        #print '========='
     pass
 
 
@@ -66,17 +70,17 @@ def enable_motor_Z():
     pass
 
 
-#TODO instead of two previous functions
+# TODO instead of two previous functions
 def rotateTo(direction):
     delay_in_millisecs_as_string = varDelay.get()
     delay_in_secs_as_float = float (varDelay.get()) /1000
     if direction == 'left':
         button_rotate_left.configure(repeatinterval = delay_in_millisecs_as_string)
-        #backwards(delay_in_secs_as_float, 1)
+        backwards(delay_in_secs_as_float, 1)
         #print 'left ' , varDelay.get(), delay_in_millisecs_as_string, delay_in_secs_as_float #for test purpose
     elif direction == 'right':
         button_rotate_right.configure(repeatinterval = delay_in_millisecs_as_string)
-        #forward(delay_in_secs_as_float, 1)
+        forward(delay_in_secs_as_float, 1)
         #print 'right ' , varDelay.get(), delay_in_millisecs_as_string, delay_in_secs_as_float #for test purpose
     
 # Buttons
@@ -86,6 +90,7 @@ button_rotate_left = Button(root, text="Turn motor left", width=20,height=5, bg=
 button_rotate_right = Button(root, text="Turn motor right", width=20,height=5, bg="blue",fg="black",
                              command = rotate_right, repeatdelay="500", repeatinterval = "1000")
 
+# Checkbuttons
 checkbutton_motor_X_enabled = Checkbutton(root, text="Expand", variable=varCheckMotor_X_Enable, command = enable_motor_X)
 
 checkbutton_motor_Y_enabled = Checkbutton(root, text="Expand", variable=varCheckMotor_Y_Enable, command = enable_motor_Y)
@@ -97,8 +102,10 @@ scaleDelay = Scale(root, from_= 1, to = 100, orient = HORIZONTAL, length = 300, 
                     variable = varDelay, command = updateAndPrintValues)
 
 # add stuff to the main board
+# TODO improve GUI
 button_rotate_left.pack(side = 'left')
 button_rotate_right.pack(side = 'right')
+
 scaleDelay.pack(side = 'bottom')
 
 checkbutton_motor_X_enabled.pack(side = 'top')
